fade349f15e155866df426021aadadf51a022f0a
t-rupe <personal@trupe.info>
2022-09-06 17:57:31 -0700

    Successfully implemented step 4&5

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function getComputerChoice() {
     let randomChoice = Math.floor(Math.random()*arrayChoice.length);
     return arrayChoice[randomChoice];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
+        return 'Tie! Try again';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+        return 'You lose! Paper beats Rock';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') { 
+        return 'Tie! Try again';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        return 'You Win! Paper beats Rock';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        return 'You Win! Scissors beats Paper';
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors') {
+        return 'Tie! Try again';   
+    }
+}
+const playerSelection = prompt('rock, paper, or scissors?');
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
